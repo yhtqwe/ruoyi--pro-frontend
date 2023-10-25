15c91c1338172894e152a24d242a8b217b5eaa31
YunaiV <zhijiantianya@gmail.com>
2023-10-25 12:48:19 +0800

    同步 1.8.3 的 vue2 前端

--- src/main/java/cn/iocoder/ui/config/UiConfiguration.java
+++ src/main/java/cn/iocoder/ui/config/UiConfiguration.java
@@ -66,6 +66,22 @@ public class UiConfiguration implements WebMvcConfigurer {
 
                 })
         ;
+
+        // 管理后台 Vue3 的配置
+        registry.addResourceHandler("/user-ui-vue2/**", "/user-ui-vue2/", "/user-ui-vue2")
+                .addResourceLocations("classpath:/static/user-ui-vue2/")
+                // 自定义 ClassPathResource 实现类，在前端请求的地址匹配不到对应的路径时，强制使用 /admin-ui-vben/index.html 资源
+                // 本质上，等价于 nginx 在处理不到 Vue 的请求地址时，try_files 到 index.html 地址
+                // 想要彻底理解，可以调试 ResourceHttpRequestHandler 的 resolveResourceLocations 方法，前端请求 /admin-ui-vben/system/tenant 地址
+                .addResourceLocations(new ClassPathResource("/static/user-ui-vue2/index.html") {
+
+                    @Override
+                    public Resource createRelative(String relativePath) {
+                        return this;
+                    }
+
+                })
+        ;
     }
 
     @Component
